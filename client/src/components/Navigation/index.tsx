e8b0b02b1d86e13c9f1b4a8c5874b58170ca4156
Jake Edgar <jakeedgar1012@gmail.com>
2022-07-28 01:23:48 -0700

    update auth

--- client/src/components/Navigation/index.tsx
+++ client/src/components/Navigation/index.tsx
@@ -1,41 +1,48 @@
-import * as React from 'react';
-import { AuthCredential, getAuth, signOut } from 'firebase/auth';
+import React, { useContext } from 'react';
+import UserContext, { initialUserState } from '../../contexts/user';
 
 export interface INavigationProps {}
 
 const Navigation: React.FC<INavigationProps> = (props) => {
-  const auth = getAuth();
+  const userContext = useContext(UserContext);
+  const { user } = userContext.userState;
 
+  const logout = () => {
+    userContext.userDispatch({ type: 'logout', payload: initialUserState });
+  };
   return (
-    <nav className="navbar-dark text-light-light-4 mb-4">
-      <h2 className="site-title">
-        <a href="/">
-          <em>wither [ai]</em>
-        </a>
-      </h2>
-      <p>an erasure poetry generator</p>
-      {AuthCredential !== null ? (
-        <ul>
-          <li>
-            <a href="/create">Create a New Poem</a>
-          </li>
-          <li>
-            <a href="#" onClick={() => signOut(auth)}>
-              Logout
-            </a>
-          </li>
-        </ul>
-      ) : (
-        <div>
+    <div className="container">
+      <nav className="navbar-dark text-light-light-4 mb-4">
+        <h2 className="site-title">
+          <a href="/">
+            <em>wither [ai]</em>
+          </a>
+        </h2>
+        <h6>an erasure poetry generator</h6>
+        {user._id !== '' ? (
           <ul>
             <li>
-              <a href="/">home</a>
+              <a href="/poems">Create a New Poem</a>
             </li>
-            <li className="ml-2 mr-2"></li>
+            <li>
+              <a onClick={() => logout()}>Logout</a>
+            </li>
           </ul>
-        </div>
-      )}
-    </nav>
+        ) : (
+          <div>
+            <ul>
+              <li>
+                <a href="/users/login">Login</a>
+              </li>
+              <li className="ml-2 mr-2"></li>
+              <li>
+                <a href="/register">Sign Up</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </nav>
+    </div>
   );
 };
 
